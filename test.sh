59519fee04a05dc3650e5bde3eb85fc565a5218e
Piotr Anielski <pr.anielski@gmail.com>
2014-10-29 13:18:03 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 DefaultWidth=20
 DefaultHeight=10
-DefaultAnts=2
-DefaultSteps=10
+DefaultAnts=10
+DefaultSteps=100
 
 Width=${1:-$DefaultWidth}
 Height=${2:-$DefaultHeight}
@@ -12,16 +12,16 @@ Steps=${4:-$DefaultSteps}
 
 echo ""
 echo "# version seq"
-erl -pa ebin -pa deps/skel/ebin -eval "parallant:start(parallant_seq, gbtree_based,$Width,$Height,$Ants,$Steps)." -run init stop -noshell
+erl -pa ebin -pa deps/skel/ebin -eval "parallant:start(parallant_seq, gbtree_based,$Width,$Height,$Ants,$Steps,false)." -run init stop -noshell
 
 echo ""
 echo "# version tiled no_skel"
-erl -pa ebin -pa deps/skel/ebin -eval "parallant:start(parallant_tiled, gbtree_based,$Width,$Height,$Ants,$Steps)." -run init stop -noshell
+erl -pa ebin -pa deps/skel/ebin -eval "parallant:start(parallant_tiled, gbtree_based,$Width,$Height,$Ants,$Steps,false)." -run init stop -noshell
 
 #start(Model, Impl, Width, Height, PopulationSize, Steps)
 
-make clean
-./rebar clean compile -D skel
-echo ""
-echo "# version tiled skel"
-erl -pa ebin -pa deps/skel/ebin -eval "parallant:start(parallant_tiled, gbtree_based,$Width,$Height,$Ants,$Steps)." -run init stop -noshell
+# make clean
+# ./rebar clean compile -D skel
+# echo ""
+# echo "# version tiled skel"
+# erl -pa ebin -pa deps/skel/ebin -eval "parallant:start(parallant_tiled, gbtree_based,$Width,$Height,$Ants,$Steps,false)." -run init stop -noshell
