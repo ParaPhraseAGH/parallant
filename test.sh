914ca9d2274b0a637f498b4d4962af6849c8c823
Piotr Anielski <pr.anielski@gmail.com>
2014-12-05 13:13:33 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -13,14 +13,15 @@ Steps=${4:-$DefaultSteps}
 algorithms="parallant_seq parallant_tiled"
 world_impls="list_based gbtree_based"
 ants_impls="ants" # ants_gbt
+models="model_langton model_forams"
 
 for algorithm in $algorithms; do
-    for world_impl in $world_impls; do
+    for model in $models; do
     	for ants_impl in $ants_impls; do
         	echo ""
-        	echo "# version $algorithm with $world_impl world_impl"
+        	echo "# version $algorithm with $model and ants stored with $ants_impl"
         	erl -pa ebin -pa deps/*/ebin \
-            	-eval "parallant:start($Width,$Height,$Ants,$Steps,[{algorithm,$algorithm},{world_impl,$world_impl},{ants_impl,$ants_impl},{log,false}])." \
+            	-eval "parallant:start($Width,$Height,$Ants,$Steps,[{algorithm,$algorithm},{model,$model},{ants_impl,$ants_impl},{log,false}])." \
             	-run init stop -noshell || exit 1
     	done
     done
