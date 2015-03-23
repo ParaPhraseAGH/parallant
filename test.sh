33901a6ddf79e7fd1ad9619d85e258c755ec53b3
dgpk <d_grzonka@poczta.onet.pl>
2015-03-23 02:25:22 +0000

    if model is not 3d then depth is equal 1

--- test.sh
+++ test.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-DefaultWidth=100
-DefaultHeight=50
+DefaultWidth=12 #100
+DefaultHeight=8 #50
 DefaultDepth=3
-DefaultAgents=100
+DefaultAgents=10 #100
 DefaultSteps=10
 
 Width=${1:-$DefaultWidth}
 Height=${2:-$DefaultHeight}
-Depth=${3:-$DefaultDepth}
+#Depth=${3:-$DefaultDepth}
 Agents=${4:-$DefaultAgents}
 Steps=${5:-$DefaultSteps}
 
 algorithms="algorithm_seq algorithm_tiled"
-agents_impls="agents_ets" #"agents_lists agents_gbtree agents_ets"
-models="model_langton model_forams model_langton3d"
+agents_impls="agents_lists agents_gbtree agents_ets"
+models="model_forams" #"model_langton model_langton3d"
 
 tiles_per_colour=4
 workers_per_colour=4
 
 for algorithm in $algorithms; do
     for model in $models; do
+        if [ $model = "model_langton3d" ] ; then
+            Depth=${3:-$DefaultDepth} ; else
+            Depth=1
+        fi
     	for agents_impl in $agents_impls; do
         	echo ""
         	echo "# version $algorithm with $model and agents stored with $agents_impl"
